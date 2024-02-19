b0aadce96b4d5e06c082eca9c1ea219f1eb0b9f2
misikir21 <misikirkaka@gmail.com>
2024-02-19 20:09:50 +0300

    add overlay

--- comp/Hero.tsx
+++ comp/Hero.tsx
@@ -20,8 +20,9 @@ function Hero() {
         />
       </div>
       <div className="hero__image-container">
-        <div className="hero__images">
+        <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
         </div>
       </div>
     </div>

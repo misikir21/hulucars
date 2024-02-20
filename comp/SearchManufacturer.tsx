4f4a2fa815dc260dd9ea4c882cc9d48f3b43fa27
misikir21 <misikirkaka@gmail.com>
2024-02-20 17:04:28 +0300

    add a transition

--- comp/SearchManufacturer.tsx
+++ comp/SearchManufacturer.tsx
@@ -1,7 +1,44 @@
+"use client";
 import React from "react";
-
-function SearchManufacturer({ manufacturer, setmanufacturer }) {
-  return <div>SearchManufacturer</div>;
+import { Combobox, Transition } from "@headlessui/react";
+import { useState, Fragment } from "react";
+import Image from "next/image";
+function SearchManufacturer({
+  manufacturer,
+  setmanufacturer,
+}: setmanufacturerprops) {
+  const [query, setquery] = useState("");
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              alt="car logo"
+              className="ml-4"
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setquery(e.target.value)}
+          />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            afterLeave={() => setquery("")}
+          >
+            <Combobox.Options></Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
 }
 
 export default SearchManufacturer;

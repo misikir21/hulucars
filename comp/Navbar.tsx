e4fbc4730aac7018ef6424d67488302d106ce81f
misikir21 <misikirkaka@gmail.com>
2024-02-20 08:49:43 +0300

    add site logo

--- comp/Navbar.tsx
+++ comp/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 function Navbar() {
   return (
-    <header className="w-full absolute- z-10">
+    <header className="w-full absolute z-10">
       <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
         <Link href="/" className="flex justify-center items-center">
           <Image
@@ -12,7 +12,7 @@ function Navbar() {
             width={118}
             height={18}
             className="object-contain"
-          ></Image>
+          />
         </Link>
       </nav>
     </header>

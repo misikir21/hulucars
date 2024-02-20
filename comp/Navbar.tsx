1c85838f307fa8b91407f3883889b7258c3bab53
misikir21 <misikirkaka@gmail.com>
2024-02-20 08:47:22 +0300

    add a header element

--- comp/Navbar.tsx
+++ comp/Navbar.tsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import Image from "next/image";
+import Link from "next/link";
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full absolute- z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src="/logo.svg"
+            alt="car hulu logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          ></Image>
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;

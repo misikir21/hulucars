495ccfcae9e5355b27d333fd612cb14ec2872fa8
misikir21 <misikirkaka@gmail.com>
2024-02-21 09:49:25 +0300

    add rapidapi endpoints

---
+++ utils/index.ts
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+const options = {
+  method: "GET",
+  url: "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+  params: { model: "corolla" },
+  headers: {
+    "X-RapidAPI-Key": "603bd22496msh73c4363424a8b5bp155f92jsn03a971d66758",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+try {
+  const response = await axios.request(options);
+  console.log(response.data);
+} catch (error) {
+  console.error(error);
+}

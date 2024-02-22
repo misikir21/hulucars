ce0783ceb87cbedf52bbe7b00f1243ef0b813699
misikir21 <misikirkaka@gmail.com>
2024-02-22 18:39:00 +0300

    add car card

--- comp/CarCard.tsx
+++ comp/CarCard.tsx
@@ -1,7 +1,20 @@
+import { CarProps } from "@/types";
 import React from "react";
 
-function CarCard() {
-  return <div>CarCard</div>;
-}
+const CarCard = ({ car, make, model, carRent }: CarProps) => {
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+      </p>
+    </div>
+  );
+};
 
 export default CarCard;

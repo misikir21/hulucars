a0f19007a4d9a90aaa13cbeb54997158b851ec3b
misikir21 <misikirkaka@gmail.com>
2024-02-22 09:10:18 +0300

    add a car card comp

--- comp/index.ts
+++ comp/index.ts
@@ -2,11 +2,13 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import CarCard from "./CarCard";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 export {
   Hero,
+  CarCard,
   SearchBar,
   SearchManufacturer,
   CustomFilter,

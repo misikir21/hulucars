02495231e6f07e03b79b4cd5c12ab35ef1798943
misikir21 <misikirkaka@gmail.com>
2024-02-23 09:26:09 +0300

    add rent price

--- comp/CarCard.tsx
+++ comp/CarCard.tsx
@@ -19,14 +19,16 @@ const CarCard = ({ car }: CarProps) => {
       </div>
       <p className="flex mt-6 text-[32px] font-extrabold">
         <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
 
         <span className="self-end text-[14px] font-medium">/day</span>
       </p>
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
           src="/hero.png"
+          width={300}
+          height={100}
           alt="car model"
-          fill
           priority
           className="object-contain"
         />

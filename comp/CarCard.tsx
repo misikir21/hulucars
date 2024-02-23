d9d804a5c2ca5020373464a5045c9585b9be7e45
misikir21 <misikirkaka@gmail.com>
2024-02-23 09:57:55 +0300

    fix all typos

--- comp/CarCard.tsx
+++ comp/CarCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import { useState } from "react";
 import { CarProps } from "@/types";
 import React from "react";
 import { CustomButton } from ".";
@@ -10,6 +11,7 @@ interface carcardprops {
 const CarCard = ({ car }: CarProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
   const carRent = calculateCarRent(city_mpg, year);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="car-card group">
       <div className="car-card__content">
@@ -17,58 +19,67 @@ const CarCard = ({ car }: CarProps) => {
           {make} {model}
         </h2>
       </div>
-      <p className="flex mt-6 text-[32px] font-extrabold">
-        <span className="self-start text-[14px] font-semibold">$</span>
-        {carRent}
 
-        <span className="self-end text-[14px] font-medium">/day</span>
+      <p className="flex mt-6 text-[32px] leading-[38px] font-extrabold">
+        <span className="self-start text-[14px] leading-[17px] font-semibold">
+          $
+        </span>
+        {carRent}
+        <span className="self-end text-[14px] leading-[17px] font-medium">
+          /day
+        </span>
       </p>
+
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
           src="/hero.png"
-          width={300}
-          height={100}
           alt="car model"
+          width={300}
+          height={150}
           priority
           className="object-contain"
         />
       </div>
+
       <div className="relative flex w-full mt-2">
-        <div className="felx group-hover:invisible w-full justify-between text-gray ">
+        <div className="flex group-hover:invisible w-full justify-between text-grey">
           <div className="flex flex-col justify-center items-center gap-2">
             <Image
               src="/steering-wheel.svg"
-              alt="steering wheel"
-              height={20}
               width={20}
+              height={20}
+              alt="steering wheel"
             />
-            <p className="text-[14]">
+            <p className="text-[14px] leading-[17px]">
               {transmission === "a" ? "Automatic" : "Manual"}
             </p>
-            <div className="flex flex-col justify-center items-center gap-2">
-              <Image src="/tire.svg" alt="tire" height={20} width={20} />
-              <p className="text-[14]">{drive.toUpperCase()}</p>
-            </div>
           </div>
-
-          <div className="flex flex-col justify-center items-center gap-2">
-            <Image src="/gas.svg" alt="gas" height={20} width={20} />
-            <p className="text-[14]">
-              {transmission === "a" ? "Automatic" : "Manual"}
-            </p>
-            <div className="flex flex-col justify-center items-center gap-2">
-              <Image src="/tire.svg" alt="tire" height={20} width={20} />
-              <p className="text-[14]">{city_mpg}MPG</p>
-            </div>
+          <div className="car-card__icon">
+            <Image src="/tire.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{drive.toUpperCase()}</p>
           </div>
-          <div className="car-card__btn-container">
-            <CustomButton
-              title="view more"
-              containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
-            />
+          <div className="car-card__icon">
+            <Image src="/gas.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{city_mpg} MPG</p>
           </div>
         </div>
+
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
       </div>
+
+      {/* <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      /> */}
     </div>
   );
 };
